69d6b8de2c9fbd4f81de45424bda0abbec91ccce
yangzhg <780531911@qq.com>
2018-02-01 17:20:55 +0800

    Update install.py

--- h2o_kernel/install.py
+++ h2o_kernel/install.py
@@ -18,7 +18,7 @@ def install_h2o_kernel_spec(user=True, prefix=None):
         os.chmod(td, 0o755) # Starts off as 700, not user readable
         with open(os.path.join(td, 'kernel.json'), 'w') as f:
             json.dump(kernel_json, f, sort_keys=True)
-        print('Installing Jupyter kernel spec')
+        print('Installing H2O kernel Jupyter kernel spec')
         KernelSpecManager().install_kernel_spec(td, 'h2o', user=user, replace=True, prefix=prefix)
 
 def _is_root():
@@ -30,11 +30,11 @@ def _is_root():
 def main(argv=None):
     ap = argparse.ArgumentParser()
     ap.add_argument('--user', action='store_true',
-        help="Install to the per-user kernels registry. Default if not root.")
+        help="Install H2O kernel to the per-user kernels registry. Default if not root.")
     ap.add_argument('--sys-prefix', action='store_true',
-        help="Install to sys.prefix (e.g. a virtualenv or conda env)")
+        help="Install H2O kernel to sys.prefix (e.g. a virtualenv or conda env)")
     ap.add_argument('--prefix',
-        help="Install to the given prefix. "
+        help="Install H2O kernel to the given prefix. "
              "Kernelspec will be installed in {PREFIX}/share/jupyter/kernels/")
     args = ap.parse_args(argv)
 
